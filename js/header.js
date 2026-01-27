647a7e6ffc34e85b59a573f55959a82736174845
yuuna-naa <yuu.na-2633@au.com>
2026-01-28 06:02:56 +0900

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -5,3 +5,25 @@ $(function(){
 });
 
 
+$(function () {
+  $('a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+
+    const speed = 400;
+    const href = $(this).attr('href');
+    const target = href === '#' || href === ''
+      ? $('html')
+      : $(href);
+
+    if (!target.length) return;
+
+    const position = target.offset().top;
+
+    $('html, body').animate(
+      { scrollTop: position },
+      speed,
+      'swing'
+    );
+  });
+});
+
